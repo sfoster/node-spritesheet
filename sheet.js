b38baf4a0ac22da9d7291f0bb120609b077fc294
Sam Foster <sam@sam-i-am.com>
2011-07-27 15:15:19 +0100

    Initial import, with test images and working prototype of the sprite-sheet image assembly tool

---
+++ sheet.js
@@ -0,0 +1,102 @@
+var sys  = require('sys');
+var fs   = require('fs');
+var path = require('path');
+var gd   = require('gd/gd');
+var promise   = require('promised-io/promise');
+
+// TODO: accept cmd arg or config for input directory
+var indir = "./test/icons", 
+	target = './test/sprite.png';
+
+// TODO: config or read source images for width/height	
+// define icons sizes
+var icon_width = 24;
+var icon_height = 24;
+
+var	files = [], 
+	sequence = null;
+	
+function run() {
+	if (path.exists(target)) 
+	  fs.unlink(target);
+
+	fs.readdir(indir, function(err, filenames){
+		files = filenames.filter(function(name){
+			return name.charAt(0) != '.' 
+				&& /\.(png)$/.test(name);
+		}).map(function(name){
+			return indir + "/" + name;
+		});
+
+		// console.log("got files: " + JSON.stringify(files));
+
+		return createSheet(files, target, {});
+	});
+}
+
+function createSheet(files, dest, options) {
+
+	// TODO: support variable heights
+	var file = null, 
+		count = files.length, 
+		i = 0, 
+		target_png = gd.createTrueColor(icon_width, icon_height * count)
+
+	console.log("target_png created");
+
+	// on object to manage an async sequence. 
+	// TODO: can use https://github.com/sfoster/dojo-samiam/blob/master/sandbox/Sequence.js ? 
+	sequence = {
+		start: function(){
+			console.log("start sequence");
+			console.log("target_png has width:height: " + icon_width +" : "+ icon_height * count);
+			this.next();
+		},
+		next: function(){
+			var file = files.shift();
+			console.log("next, with: " + file);
+			if(file) {
+				copyIntoImage(target_png, file, i++)
+					.then(sequence.next);
+			} else {
+				sequence.atlast();
+			}
+		},
+		atlast: function(){
+			// finally 
+			// target_png.savePng(target, 1, gd.noop);
+
+			// create our final output image.
+			var result = target_png.savePng(target, 1, gd.noop);
+			console.log("wrote output to: " + target);
+		}.bind(this)
+	};
+	sequence.start();
+}
+
+function copyIntoImage(target_png, sourcefile, i){
+	
+	var pr = promise.Promise();
+	gd.openPng(sourcefile, function(sourcePng, path) {
+		// console.log("source img: " + sourcefile + ", copy to: 0/"+ (i * icon_height));
+		
+		sourcePng.copy(
+			target_png, // into where (?)
+			0,	// destination x
+			i * icon_height,	// destination y
+			0,	// source x
+			0,	// source y
+			icon_width, // width/height to copy
+			icon_height
+		);
+
+		// release mem for the source icon img
+		sourcePng.destroy();
+		pr.resolve(path + " copied");
+	});
+	return pr;
+}
+
+if(require.main === module) {
+	run();
+}
